26012327f93be4b52402b717abedd29223f72a74
dgusfr <diegogustavo.fr@gmail.com>
2024-12-05 21:50:57 -0300

    feat main go

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/Alura/routes"
 )
 
 func main() {
+	// Carrega as rotas da aplicação
 	routes.CarregaRotas()
-	http.ListenAndServe(":8000", nil)
+
+	// Define a porta e inicia o servidor
+	port := ":8000"
+	log.Printf("Servidor iniciado na porta %s", port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatalf("Erro ao iniciar o servidor: %v", err)
+	}
 }
