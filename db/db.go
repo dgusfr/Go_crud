575cde30e9250b1641f54694fa16bc863d904fb4
dgusfr <diegogustavo.fr@gmail.com>
2024-12-05 21:54:40 -0300

    feat database

--- db/db.go
+++ db/db.go
@@ -2,15 +2,24 @@ package db
 
 import (
 	"database/sql"
+	"log"
 
 	_ "github.com/lib/pq"
 )
 
+// ConectaComBancoDeDados retorna uma conexão ativa com o banco de dados PostgreSQL
 func ConectaComBancoDeDados() *sql.DB {
-	conexao := "user=root dbname=go_crud password=1234 host=localhost sslmode=disable"
-	db, err := sql.Open("postgres", conexao)
+	dsn := "user=root dbname=go_crud password=1234 host=localhost sslmode=disable"
+
+	db, err := sql.Open("postgres", dsn)
 	if err != nil {
-		panic(err.Error())
+		log.Fatalf("Erro ao abrir conexão com o banco de dados: %v", err)
 	}
+
+	// Testa a conexão com o banco de dados
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Erro ao conectar ao banco de dados: %v", err)
+	}
+
 	return db
 }
